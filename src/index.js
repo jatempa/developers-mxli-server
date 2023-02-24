8f42a2233465984dd068a9a2a22a60da1bf79a82
Jorge Atempa <atempaitm@gmail.com>
2023-02-23 20:56:28 -0800

    Create route to get profiles

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const { getProfiles } = require('./controllers/profileController.js');
+const router = require('./routes/routes');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  getProfiles(req, res);
-});
+app.use('/api', router);
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
---
+++ src/routes/routes.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+const router = Router();
+const { getProfiles } = require('./controllers/profileController.js');
+
+router.get('/profiles', (req, res) => {
+  getProfiles(req, res);
+});
+
+module.exports = { router };
