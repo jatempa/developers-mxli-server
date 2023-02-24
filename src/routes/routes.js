f11a445f22ea37bde418e8e1bdbe1b34a9fe917d
Jorge Atempa <atempaitm@gmail.com>
2023-02-23 21:06:02 -0800

    Update route to get profiles data

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-const { getProfiles } = require('./controllers/profileController.js');
+const { getProfiles } = require('../controllers/profileController.js');
 
 router.get('/profiles', (req, res) => {
   getProfiles(req, res);
