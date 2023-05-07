8ab17cc69815810d93d8358e3a1686b9e0fb7448
Jorge Atempa <atempaitm@gmail.com>
2023-05-06 22:58:31 -0700

    Create routes to manage user

---
+++ src/handlers/user.ts
@@ -0,0 +1,48 @@
+import { comparePasswords, createJWT, hashPassword } from '../modules/auth';
+import { NextFunction, Request, Response } from 'express';
+
+const createNewUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    console.log(req);
+    // const user = await prisma.user.create({
+    //   data: {
+    //     username: req.body.username,
+    //     password: await hashPassword(req.body.password),
+    //   },
+    // });
+    // const token = createJWT(user);
+    // res.json({ token });
+    res.send('OK SUCCESS ');
+  } catch (e) {
+    next(e);
+  }
+};
+
+const signIn = async (req: Request, res: Response) => {
+  try {
+    // const user = await prisma.user.findUnique({
+    //   where: {
+    //     username: req.body.username,
+    //   },
+    // });
+    // const isValid = await comparePasswords(req.body.password, user.password);
+    // if (!isValid) {
+    //   res.status(401);
+    //   res.json({ message: 'nope' });
+    //   return;
+    // }
+    // const token = createJWT(user);
+    // res.json({ token });
+  } catch (e) {
+    console.error(e);
+    res.status(401);
+    res.json({ message: 'not valid user' });
+    return;
+  }
+};
+
+export { createNewUser, signIn };
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import express from 'express';
 import router from './router/router';
 import cors from 'cors';
+import { createNewUser, signIn } from './handlers/user';
 // CONSTANTS
 const PORT = process.env.PORT || 8080;
 // enable modules
@@ -11,6 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/api', router);
+// Create user
+app.post('/user', createNewUser);
+// Sign in
+app.post('/signIn', signIn);
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
